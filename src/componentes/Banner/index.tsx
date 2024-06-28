e654b127d6d6ebf303717c4f0b2ee3c90d8e0f2a
pedroquintao <pedro.h.quintao@hotmail.com>
2024-06-28 13:44:07 -0300

    Commit para tentativa de deploy pela vercel

--- src/componentes/Banner/index.tsx
+++ src/componentes/Banner/index.tsx
@@ -1,13 +1,10 @@
 import "./Banner.css";
-
-import React from "react";
-
 interface BannerProps {
   enderecoImagem: string;
   textoAlternativo?: string;
 }
 
-export const Banner = ({enderecoImagem, textoAlternativo} :BannerProps) => {
+export const Banner = ({ enderecoImagem, textoAlternativo }: BannerProps) => {
   // JSX
   return (
     <header className="banner">
